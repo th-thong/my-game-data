bbf91918eccf8a1d831fb5ed85211b829d63d480
Trần Thanh Thông <thongtran.dev@gmail.com>
2026-03-06 11:55:21 +0700

    feat: add create, get weapon data api

--- graph/model/model.go
+++ graph/model/model.go
@@ -10,3 +10,15 @@ type NewCharacterData struct {
     Name    string `json:"name" bson:"name"`
     RoundIcon string `json:"round_icon" bson:"round_icon"`
 }
+
+type Weapon struct {
+    ID      string `json:"id" bson:"_id,omitempty"`
+    Name    string `json:"name" bson:"name"`
+    RoundIcon string `json:"round_icon" bson:"round_icon"`
+}
+
+type NewWeaponData struct {
+    Name    string `json:"name" bson:"name"`
+    RoundIcon string `json:"round_icon" bson:"round_icon"`
+}
+
